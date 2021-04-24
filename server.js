91ee39cd3031e141e1bdff606add07a1bff49bd8
Manuel <alessimanuel98@gmail.com>
2021-04-24 21:50:42 +0200

    Server con accesso a facebook

---
+++ server.js
@@ -0,0 +1,96 @@
+var express = require('express');
+var app = express();
+var fs = require('fs');
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
+const path = require('path');
+var request = require('request');
+require('dotenv').config()
+//let rawdata = fs.readFileSync('secrets.json');
+//let sec = JSON.parse(rawdata);
+var token="";
+var code="";
+
+app.get('/log',function (req,res){
+  res.redirect("https://www.facebook.com/v10.0/dialog/oauth?client_id="+process.env.CLIENT_ID+"&redirect_uri=http://localhost:8000/pretoken&response_type=code&scopes=user_birthday&user_hometown&user_gender");
+});
+
+
+
+app.get('/pretoken', function (req,res){
+  //console.log(req);
+  code=req.query.code;
+  res.send("Got the code: "+code+"<br><br> And the token:"+token+"<br><br><button onclick='window.location.href=\"/token\"'>Get TOKEN</button>"+
+  "<br><br><button onclick='window.location.href=\"/api\"'>Get User Info</button>");
+});
+
+app.get('/token',function (req,res){
+  //code=req.query.code;
+  var formData = {
+    code: code,
+    client_id: process.env.CLIENT_ID,
+    client_secret: process.env.SECRET,
+    redirect_uri: "http://localhost:8000/pretoken",
+    grant_type: 'authorization_code'
+  }
+  request.post({url:'https://graph.facebook.com/v10.0/oauth/access_token?', form: formData}, function callback(err, httpResponse, body) {
+
+    if (err) {
+      return console.error('upload failed:', err);
+    }
+    //console.log('Upload successful!  Server responded with:', body);
+    var info = JSON.parse(body);
+    token = info.access_token;
+    res.redirect('/pretoken');
+  });
+});
+
+app.get('/api',function (req,res){
+
+  var url = 'https://graph.facebook.com/me?fields=id,name,birthday,hometown,gender&access_token='+token
+        var headers = {'Authorization': 'Bearer '+token};
+        
+      
+        var request = require('request');
+    
+    
+        request.get({
+            headers: headers,
+            url:     url,
+            }, function(error, response, body){
+                console.log(body);
+                body2 = JSON.parse(body)
+                res.send("Nome:"+body2.name
+                +"<br>Birthday: "+body2.birthday
+                +"<br>Hometown: "+body2.hometown.name
+                +"<br>Gender: "+body2.gender);
+            });
+
+});
+
+
+
+
+
+
+app.get('/',function (req,res){
+  res.sendFile(path.resolve('index.html'));
+});
+
+app.get('/indexbis.html',function (req,res){
+  res.sendFile(path.resolve('indexbis.html'));
+});
+
+
+app.get('/bootstrap.min.css',function (req,res){
+  res.sendFile(path.resolve('bootstrap.min.css'));
+});
+
+
+
+var server = app.listen(8000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+  
+  console.log('Server listening at http://%s:%s', host, port);
+});
