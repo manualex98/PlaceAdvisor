df81b2263d57329c1a2dcf963d041c57ce25d54b
Manuel <alessimanuel98@gmail.com>
2021-04-24 22:55:00 +0200

    Fix generali

--- server.js
+++ server.js
@@ -12,16 +12,14 @@ var token="";
 var code="";
 
 app.get('/log',function (req,res){
-  res.redirect("https://www.facebook.com/v10.0/dialog/oauth?client_id="+process.env.CLIENT_ID+"&redirect_uri=http://localhost:8000/pretoken&response_type=code&scopes=user_birthday&user_hometown&user_gender");
+  res.redirect("https://www.facebook.com/v10.0/dialog/oauth?client_id="+process.env.CLIENT_ID+"&redirect_uri=http://localhost:8000/homepage&response_type=code&scopes=user_birthday&user_hometown&user_gender");
 });
 
 
 
-app.get('/pretoken', function (req,res){
-  //console.log(req);
+app.get('/homepage', function (req,res){
   code=req.query.code;
-  res.send("Got the code: "+code+"<br><br> And the token:"+token+"<br><br><button onclick='window.location.href=\"/token\"'>Get TOKEN</button>"+
-  "<br><br><button onclick='window.location.href=\"/api\"'>Get User Info</button>");
+  res.sendFile(path.resolve('homepage.html'));
 });
 
 app.get('/token',function (req,res){
@@ -30,7 +28,7 @@ app.get('/token',function (req,res){
     code: code,
     client_id: process.env.CLIENT_ID,
     client_secret: process.env.SECRET,
-    redirect_uri: "http://localhost:8000/pretoken",
+    redirect_uri: "http://localhost:8000/homepage",
     grant_type: 'authorization_code'
   }
   request.post({url:'https://graph.facebook.com/v10.0/oauth/access_token?', form: formData}, function callback(err, httpResponse, body) {
@@ -41,7 +39,7 @@ app.get('/token',function (req,res){
     //console.log('Upload successful!  Server responded with:', body);
     var info = JSON.parse(body);
     token = info.access_token;
-    res.redirect('/pretoken');
+    res.redirect('/api');
   });
 });
 
@@ -49,11 +47,8 @@ app.get('/api',function (req,res){
 
   var url = 'https://graph.facebook.com/me?fields=id,name,birthday,hometown,gender&access_token='+token
         var headers = {'Authorization': 'Bearer '+token};
-        
-      
         var request = require('request');
-    
-    
+
         request.get({
             headers: headers,
             url:     url,
@@ -75,10 +70,6 @@ app.get('/api',function (req,res){
 
 app.get('/',function (req,res){
   res.sendFile(path.resolve('index.html'));
-});
-
-app.get('/indexbis.html',function (req,res){
-  res.sendFile(path.resolve('indexbis.html'));
 });
 
 
