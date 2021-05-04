1da303c827b2e007d64a457891bcb3ec5b6169de
francesco-fortunato <francesco.fortunato1999@gmail.com>
2021-05-04 15:14:38 +0200

    cancellato codice x pagine html

--- server.js
+++ server.js
@@ -184,42 +184,6 @@ app.get('/app', function(req,res){
     n = data.length;
     res.render('list_places', {numero: n, data: data, cat: cate, citta: city});
   })
- 
-  
-  
-  
-  
-  
-  
-  //SENZA EJS
-  /* var page="<h1>First 100 results for "+cate+" in "+city+"</h1><ul>";
-
-  var options = {
-    url: 'https://api.opentripmap.com/0.1/en/places/radius?format=geojson&apikey='+process.env.OpenMap_KEY+'&radius='+rad+'&lon='+lon+'&lat='+lat+'&kinds='+cate+'&limit='+100
-  }
-  
-  request.get(options,function callback(error, response, body){
-    //console.log(body)
-    var info = JSON.parse(body);
-    var data = info.features;
-    var n = data.length;
-
-    for(var i=0; i<n; i++){
-      
-      var details="<a href=http://localhost:8000/details?xid="+data[i].properties.xid+">Details</a>"
-
-      if(data[i].properties.name != "") page += "<li><h3>"+data[i].properties.name+"</h3></li>"+details+"<br>"
-      //else page += "<li><h3>This place hasn't a name</h3></li><br>"
-    }
-
-    page += "</ul>"
-    page+='<a href="http://localhost:8000/homepage" >Go Back</a>'
-    res.send(page);
-    page="";
-  });
-*/
-
-
 });
 
 
@@ -280,49 +244,7 @@ app.get('/reviews', function(req,res){
           res.redirect('/details?xid='+xid);
       }
   });
-
-  
 });
-
-    //Senza ejs
-    /*
-    var via;
-    if(info.address.road==undefined) via = info.address.pedestrian
-    else via = info.address.road
-
-    var civico;
-    if(info.address.house_number==undefined) civico="SNC"
-    else civico= info.address.house_number
-    
-    var FBbutton1 = '<div id="fb-root"></div><script async defer crossorigin="anonymous" src="https://connect.facebook.net/it_IT/sdk.js#xfbml=1&version=v10.0&appId=468739614360394&autoLogAppEvents=1" nonce="iFPJ5Fwi"></script>'
-    
-    var image="No images available"
-    if(info.preview!=undefined){
-    image='<img src="'+info.preview.source+'" width="'+info.preview.width+'" height="'+info.preview.height+'">'}
-
-    var page=FBbutton1;
-    page+="<h1>"+info.name+"</h1><br><h2>Details:</h2>"+image+"<br><ul>";
-    
-    var background = "No background available"
-    if(info.wikipedia_extracts!=undefined){
-      background = info.wikipedia_extracts.html
-    }
-    page += "<li><h3><b><i>You can find it at: </b></h3>"+via+"<b> number: </b>"+civico+"</i></li><br>"+
-    "<li><h3>Background: </h3><i>"+background+"</i></li><br>"
-    
-    var link = 'http://127.0.0.1:8000/details?xid='+xid;
-    
-    var FBbutton2 = '<div class="fb-share-button" data-href="'+link+'" data-layout="button" data-size="large"><a target="_blank" href="http://www.facebook.com/sharer/sharer.php?u=http%3A%2F%2F127.0.0.1%2F8000%2F'+'details?xid='+xid+'%3A8000%2F&amp;src=sdkpreparse" class="fb-xfbml-parse-ignore">Share</a></div>'
-    
-    page+="<p>Share this place with your friends</p><br>"
-    page+=FBbutton2;
-    
-    page += "</ul>"
-    res.send(page);
-    page="";
-  });
-*/
-
 
 app.get('/',function (req,res){
   res.render('index.ejs',);
